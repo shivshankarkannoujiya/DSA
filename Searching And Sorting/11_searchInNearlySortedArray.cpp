3d65528df5390443f95021c8f67e7e6020523803
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-10-08 14:15:46 +0530

    Addes sol_of_searchInNearlySortedArra

---
+++ Searching And Sorting/11_searchInNearlySortedArray.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int searchInNearlySortedArray(int arr[], int n, int target)
+{
+
+    int start = 0;
+    int end = n - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+
+        if (mid - 1 >= 0 && arr[mid - 1] == target)
+        {
+            return mid - 1;
+        }
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        if (mid + 1 <= n && arr[mid + 1] == target)
+        {
+            return mid + 1;
+        }
+
+        if (target > arr[mid])
+        {
+
+            // goto right
+            start = mid + 2;
+        }
+        else
+        {
+
+            // goto left
+            end = mid - 1;
+        }
+
+        // update mid
+        mid = start + (end - start) / 2;
+    }
+
+    // not found
+    return -1;
+}
+
+int main()
+{
+
+    // int arr[] = {20, 10, 30, 50, 40, 70, 60};
+    int arr[] = {20, 30};
+    int n = 7;
+    int target;
+    cout << "Enter Target : ";
+    cin >> target;
+
+    // calling function
+    int ansIndex = searchInNearlySortedArray(arr, n, target);
+    if (ansIndex == -1)
+    {
+        cout << "Not Found !!!" << endl;
+    }
+    else
+    {
+        cout << "Element found at index : " << ansIndex << endl;
+    }
+}
