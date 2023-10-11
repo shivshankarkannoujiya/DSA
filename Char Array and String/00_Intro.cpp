d1ce4b2a3bdb854f63cae5a5d177fc32128e72c6
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-10-11 18:26:41 +0530

    Addes char Array

--- Char Array and String/00_Intro.cpp
+++ Char Array and String/00_Intro.cpp
@@ -1,8 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
- 
-    return 0;
+
+    // Creating char array
+    cout << "Enter the Character : ";
+    char ch[5];
+
+    // Taking input Array
+    // cin >> ch;
+
+    // Printing Char Array
+    cout << ch;
+
+    // Accessing
+    // cout << ch[0] << endl;
+    // cout << ch[1] << endl;
+    // cout << ch[2] << endl;
+    // cout << ch[3] << endl;
+    // cout << ch[4] << endl;
+
+    // Printing using loop
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "Char at" << i << " is  : " << ch[i] << endl;
+    // }
+
+    // ASCII value of NULL char [\0] : 0
+    // Verifying
+
+    // char temp = ch[4];
+    // typecasting it into int
+    // int value = (int)temp;
+    // cout << "Printing integer value : " << value;  // Ans : 0 verified
+
+    char name[100];
+    // cin >> name;
+    // cout << name;
+
+    // Using cin.getline()
+    cin.getline(name, 90);
+    cout << name;
 }
