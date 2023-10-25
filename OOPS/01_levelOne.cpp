f3b7e9406f19c8c1766af7ba129faf3e7a190f19
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-10-25 20:06:29 +0530

    level-1

---
+++ OOPS/01_levelOne.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+    Have to print the students names,id's,nos etc..
+    int id1, id2;
+    string name1, name2;
+    int age1, age2;
+    int noS1, noS2;
+    int marks[5], marks[5];
+    */
+}
