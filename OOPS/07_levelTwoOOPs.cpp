74bd662d4267e357328fcc34d92c3ee6a3bb21ae
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2023-10-29 22:39:17 +0530

    OOPs_level-2

---
+++ OOPS/07_levelTwoOOPs.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+
+private:
+    int pass;
+
+public:
+    string name;
+    int age;
+    int id;
+    int noS;
+
+    void Study()
+    {
+        cout << "Studying" << endl;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
