949f36a521d7028fbc66e817f3806618ad8ca8c0
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2026-01-14 18:24:11 +0530

    add implementation of insertion sort

---
+++ 01Array/09InsertionSort.py
@@ -0,0 +1,10 @@
+def insertion_sort(arr):
+    n = len(arr)
+
+
+
+
+if __name__ == "__main__":
+    arr = [70, 20, 50, 30, 90, 5, 15]
+    result = insertion_sort(arr)
+    print("Array after sorting: ", result)
